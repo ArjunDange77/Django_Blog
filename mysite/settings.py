5c87bf25bca5f201286532963545fc0119b6c58d
Arjun <arjundange77@gmail.com>
2023-04-02 22:39:09 +0530

    Submit Post as Email functionality added

--- mysite/settings.py
+++ mysite/settings.py
@@ -132,3 +132,13 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'assets')
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
+# Will be different for different users , change
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER = 'your_account@gmail.com'
+EMAIL_HOST_PASSWORD = 'your_password'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+
