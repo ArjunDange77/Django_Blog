4891a2d51ab836f87fd8b7196c3ce701c699933f
Arjun <arjundange77@gmail.com>
2023-04-04 21:29:47 +0530

    Adding tags below the post

--- blog/views.py
+++ blog/views.py
@@ -4,14 +4,8 @@ from django.core.paginator import Paginator, EmptyPage,PageNotAnInteger
 from django.views.generic import ListView
 from .forms import EmailPostForm , CommentForm
 from django.core.mail import send_mail
+from taggit.models import Tag
 
-  
-
-def post_list(request):
-    posts = Post.published.all()
-    return render(request,
-                'blog/post/list.html',
-                {'posts': posts})
 
 def post_detail(request, year, month, day, post):
        post = get_object_or_404(Post, slug=post,
@@ -38,6 +32,24 @@ def post_detail(request, year, month, day, post):
                      'blog/post/detail.html',
                      {'post': post,'comments': comments, 'new_comment': new_comment, 'comment_form': comment_form})
 
+
+def post_list(request, tag_slug=None):
+    object_list = Post.published.all()
+    tag = None
+    if tag_slug:
+        tag = get_object_or_404(Tag, slug=tag_slug)
+        object_list = object_list.filter(tags__in=[tag])
+
+    paginator = Paginator(object_list, 3) 
+    page = request.GET.get('page')
+    try:
+        posts = paginator.page(page)
+    except PageNotAnInteger:
+        
+        posts = paginator.page(1)
+    except EmptyPage:
+        posts = paginator.page(paginator.num_pages)
+    return render(request, 'blog/post/list.html', {'page': page, 'posts': posts, 'tag': tag})
 
 def post_list(request):
     object_list = Post.published.all()
